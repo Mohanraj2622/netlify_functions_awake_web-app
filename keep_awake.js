f8a07aaac09c48e20fa5919c6cdfc3aa972eec97
Mohanraj M <167312874+Mohanraj2622@users.noreply.github.com>
2024-05-21 16:01:39 +0530

    Create keep_awake.js

---
+++ keep_awake.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+    const url = "https://product-recommendation-rsfm.onrender.com";
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: 'Ping successful!' }),
+            };
+        } else {
+            return {
+                statusCode: response.status,
+                body: JSON.stringify({ message: 'Ping failed' }),
+            };
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Ping failed', error: error.message }),
+        };
+    }
+};
